51dccde74101427e5e6540c67718a2a240e10ad9
ChinniRajulapati <rchinni98@gmail.com>
2023-03-12 09:24:35 +0000

    first commit

---
+++ src/components/LogoutButton/index.js
@@ -0,0 +1,20 @@
+import {withRouter} from 'react-router-dom'
+import Cookies from 'js-cookie'
+
+import './index.css'
+
+const LogoutButton = props => {
+  const onClickLogout = () => {
+    const {history} = props
+    Cookies.remove('jwt_token')
+    history.replace('/login')
+  }
+  return (
+    <div className="button-container">
+      <button onClick={onClickLogout} type="button">
+        Logout
+      </button>
+    </div>
+  )
+}
+export default withRouter(LogoutButton)
